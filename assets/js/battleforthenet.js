e50aa68abcc23dfefd51945af2bf9151f1ee489a
spikespaz <spikespaz@outlook.com>
2017-10-18 17:18:05 -0700

    Add custom script for BFTN

---
+++ assets/js/battleforthenet.js
@@ -0,0 +1,12 @@
+var _bftn_options = {
+    theme: "without",
+    org: 'fftf',
+    delay: 2000,
+    date: new Date(2017, 10, 23),
+    viewCookieExpires: 0.16,
+    actionCookieExpires: 7,
+    uncloseable: false,
+    disableGoogleAnalytics: false,
+    always_show_widget: false
+};
+
